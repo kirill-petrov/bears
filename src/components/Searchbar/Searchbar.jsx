6c09b5f0582b1ab6b6e9fdcfb2b5ed312a0090bb
kirill petrov <mail@kirill-petrov.ru>
2022-06-16 23:29:18 +0300

    stable

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -4,13 +4,11 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import RefreshIcon from '@mui/icons-material/Refresh';
-import DateRangeRoundedIcon from '@mui/icons-material/DateRangeRounded';
 import ScheduleRoundedIcon from '@mui/icons-material/ScheduleRounded';
 
 export default function Searchbar() {
