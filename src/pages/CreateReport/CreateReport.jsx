d608ea0eb254a8813be0a075aaeaa93ccc471c02
kirill petrov <mail@kirill-petrov.ru>
2022-06-22 16:13:22 +0300

    added form on page createReport

--- src/pages/CreateReport/CreateReport.jsx
+++ src/pages/CreateReport/CreateReport.jsx
@@ -1,11 +1,78 @@
-import React from 'react';
+import { Button, Paper, TextField } from '@mui/material';
+import { KeyboardArrowRightOutlined } from '@mui/icons-material';
+import React, { useState } from 'react';
 import { Navigation } from '../../components';
+import { reportInputs as inputs } from './formSource.js';
+import './createReport.scss';
 
 export default function CreateReport() {
+  const [data, setData] = useState('x');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const {
+      customer,
+      executor,
+      object,
+      machine,
+      driver,
+      notes,
+      arrival,
+      departure,
+      lunch,
+    } = e.target;
+
+    setData({
+      customer: customer.value,
+      executor: executor.value,
+      object: object.value,
+      machine: machine.value,
+      driver: driver.value,
+      notes: notes.value,
+      arrival: arrival.value,
+      departure: departure.value,
+      lunch: lunch.value,
+    });
+  };
+
   return (
     <div className="create-report">
       <Navigation />
-      <div className="container"></div>
+      <Paper sx={{ maxWidth: 936, margin: 'auto', overflow: 'hidden' }}>
+        <h1 className="title">Рапорт о работе техники</h1>
+
+        <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+          <p>
+            <sup>*</sup>&nbsp;Поля со звёздочкой, являются обязательными для
+            заполнения
+          </p>
+          {inputs.map((item, i) => (
+            <TextField
+              key={i}
+              name={item.name}
+              type={item.type}
+              required={item.required}
+              label={item.label}
+              defaultValue={item.defaultValue ? item.defaultValue : null}
+              helperText={item.helperText ? item.helperText : null}
+              multiline={item.multiline ? true : false}
+              rows={item.rows ? item.rows : null}
+              variant="outlined"
+              fullWidth
+            />
+          ))}
+          <div className="form-btn">
+            <Button
+              type="submit"
+              variant="outlined"
+              endIcon={<KeyboardArrowRightOutlined />}
+            >
+              Отправить
+            </Button>
+          </div>
+        </form>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </Paper>
     </div>
   );
 }
---
+++ src/pages/CreateReport/formSource.js
@@ -0,0 +1,85 @@
+const localDateTime = () => {
+  const date = new Date();
+  return new Date(
+    date.getTime() - date.getTimezoneOffset() * 60000
+  ).toISOString();
+};
+
+export const reportInputs = [
+  {
+    name: 'customer',
+    required: true,
+    label: 'ФИО заказчика полностью (Иванов Иван Иванович)',
+    type: 'text',
+  },
+  {
+    name: 'executor',
+    required: true,
+    label: 'ФИО исполнителя полностью (Иванов Иван Иванович)',
+    type: 'text',
+  },
+  {
+    name: 'object',
+    required: true,
+    label: 'Объект',
+    type: 'text',
+  },
+  {
+    name: 'machine',
+    required: true,
+    label: 'Наименование и марка техники',
+    type: 'text',
+  },
+  {
+    name: 'driver',
+    required: true,
+    label: 'ФИО водителя/машиниста полностью (Иванов Иван Иванович)',
+    type: 'text',
+  },
+  {
+    name: 'notes',
+    required: false,
+    label: 'Примечания',
+    type: 'text',
+    multiline: true,
+    rows: 2,
+  },
+
+  {
+    name: 'arrival',
+    required: true,
+    type: 'datetime-local',
+    label: 'Время прибытия',
+    defaultValue: localDateTime().slice(0, 16),
+  },
+  {
+    name: 'departure',
+    required: true,
+    type: 'datetime-local',
+    label: 'Время убытия',
+    defaultValue: localDateTime().slice(0, 16),
+  },
+  {
+    name: 'lunch',
+    required: false,
+    type: 'number',
+    label: 'Время обеда, мин.',
+  },
+  // {
+  //   required: true,
+  //   label: 'Подпись заказчика',
+  //   type: 'text',
+  //   defaultValue: 'Подпись заказчика',
+  // },
+  // {
+  //   required: true,
+  //   label: 'Подпись тсполнителя',
+  //   type: 'text',
+  //   defaultValue: 'Подпись тсполнителя',
+  // },
+  // {
+  //   required: false,
+  //   label: 'Итого отработано',
+  //   type: 'text',
+  // },
+];
